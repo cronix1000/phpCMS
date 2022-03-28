b8db4971047fd8496258af17e9550c68cb78fbfe
Aleks4920 <crackenyogs10@gmail.com>
2022-03-27 23:36:56 -0400

    first commit

---
+++ require/db.php
@@ -0,0 +1,5 @@
+<?php
+// global db connection for all pages
+$db = new PDO('mysql:host=172.31.22.43;dbname=Idan1169347', 'Idan1169347','xGczWvXz_e');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
